8a95d77148531e5a6b5f9817f2c4b778be71dffe
haliphax <haliphax@gmail.com>
2016-02-21 11:15:15 -0600

    do not use db before it is created

--- divvy/__init__.py
+++ divvy/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from sqlalchemy.exc import OperationalError
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,7 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['DEBUG'] = True
 app.secret_key = os.getenv('SECRET_KEY', 'changemeplease')
 db = SQLAlchemy(app)
-__all__ = ["db", "app"]
 
 #configure authentication
 login_manager = LoginManager()
@@ -21,5 +21,10 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-import views
 import models
+
+# @HACK to avoid views loading during initdb stage
+try:
+    import views
+except OperationalError:
+    pass
